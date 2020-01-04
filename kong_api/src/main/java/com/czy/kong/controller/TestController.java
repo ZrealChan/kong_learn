85af027d12beddd3a0ecc3680c947e107e7e3225
chenzhiyong <chenzhy1@corp.21cn.com>
2020-01-04 17:04:33 +0800

    update api

--- kong_api/src/main/java/com/czy/kong/controller/TestController.java
+++ kong_api/src/main/java/com/czy/kong/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.czy.kong.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +19,15 @@ public class TestController {
     @RequestMapping("/learn")
     @ResponseBody
     public String learn(HttpServletRequest request, HttpServletResponse response) {
-        return "the api is success";
+        String arg = request.getParameter("key");
+        JSONObject result = new JSONObject();
+        if (arg != null && arg.length() > 0) {
+            result.put("result", 0);
+        } else {
+            result.put("result", -1);
+        }
+        result.put("msg", "the api is success, and key is " + arg);
+        return result.toJSONString();
     }
 
     @RequestMapping("/status")
